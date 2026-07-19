1d28a06bad896cf9af2481dbb362246f07d18581
prxtyushaggarwal <pratyushaggarwal2110@gmail.com>
2026-07-19 10:25:17 +0530

    Update CinandCerr.cpp

--- c++/CinandCerr.cpp
+++ c++/CinandCerr.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 #include<string>            //   WE USE <STRING> TO STORE STRINGS IN CPP
 int main(){
-   /* int age ;
+   int age ;
     std::string name;
     std::cout<<"please enter your name and age"<<std::endl;
     std::cin>>name>>age;
     std::cout<<"hello "<<name<<" you are "<< age<<" years old!";
     return 0;
-*/
+
     int age1;
     std::string full_name;
     std::cout<<"please enter your name and age"<<std::endl;
