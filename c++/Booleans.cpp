f40ab0813ad458de9745cce3cf99b5b8e1d9782b
prxtyushaggarwal <pratyushaggarwal2110@gmail.com>
2026-07-19 10:24:32 +0530

    Update Booleans.cpp

--- c++/Booleans.cpp
+++ c++/Booleans.cpp
@@ -13,5 +13,18 @@ int main(){
     }else{
         std::cout<<"the light is green"<<std::endl;
     }
+    std::cout<<std::boolalpha<<std::endl;       //BOOLALPHA CONVERTS THE DEFAULT 0,1 OUTPUT INTO TRUE OR FALSE
+    if(red_light   ){
+        std::cout<<bool (red_light)<<std::endl;
+     }else{
+        std::cout<<bool (!red_light)<<std::endl;
+    
+        }
+        if(!green_light){
+            std::cout<<bool(green_light)<<std::endl;
+    }else
+    {
+        std::cout<<bool(!green_light)<<std::endl;
+    }
     return 0;
 }
